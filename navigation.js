23acfc63b6fcf478f17cea83b65e67cf6d9a4664
Arctifice <730282@student.inholland.nl>
2024-10-09 13:21:55 +0200

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -8,19 +8,19 @@
         case 'Home':
           window.location.href = './index.html';
           break;
-        case 'Essential Components':
+        case 'Essential Components ▼':
           window.location.href = './essential-components.html';
           break;
-        case 'Comparing Components':
+        case 'Comparing Components ▼':
           window.location.href = './comparing-components.html';
           break;
-        case 'Assembling the Computer':
+        case 'Assembling the Computer ▼':
           window.location.href = './assembling-computer.html';
           break;
-        case 'Troubleshooting':
+        case 'Troubleshooting ▼':
           window.location.href = './troubleshooting.html';
           break;
-        case 'Contact and FAQs':
+        case 'Contact and FAQs ▼':
          window.location.href = './contact.html';
           break;
         default:
